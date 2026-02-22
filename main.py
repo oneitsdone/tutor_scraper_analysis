b4f9f8cfbf1e921dd10b62735986be7f036c56e3
oneitsdone <86693303+oneitsdone@users.noreply.github.com>
2026-02-22 18:17:03 -0500

    Base of Webapp
    This is just the sidebar as well as the pages

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-main
+import streamlit as st
+
+def main():
+    
+    about_website = st.Page(page="mult_pages/about_website.py", 
+                            title="About",
+                              icon=":material/info:",
+                              default=True)
+    
+    analytics_page = st.Page(page="mult_pages/analytics_page.py",
+                             title="Analytics",
+                             icon=":material/insert_chart:")
+    
+    webscraper = st.Page(page="mult_pages/webscraper.py",
+                         title="Tutor Scraper",
+                         icon=":material/data_exploration:")
+    
+    pg = st.navigation(pages=[about_website, analytics_page, webscraper])
+    pg.run()
+
+main()
